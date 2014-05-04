1c8150967abad5e510f5fad4d95269d73ee278db
Felix Huang <gzfelix@gmail.com>
2014-05-04 14:30:02 -0400

    added - cc3000 connection

--- sketches/home_monitor/home_monitor.ino
+++ sketches/home_monitor/home_monitor.ino
@@ -2,9 +2,11 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP085_U.h>
 #include <Adafruit_TSL2561_U.h>
+
 #include <Adafruit_CC3000.h>
 #include <ccspi.h>
 #include <SPI.h>
+#include <string.h>
 
 // These are the interrupt and control pins
 #define ADAFRUIT_CC3000_IRQ   3  // MUST be an interrupt pin!
@@ -15,7 +17,13 @@
 // On an UNO, SCK = 13, MISO = 12, and MOSI = 11
 Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT,
                                          SPI_CLOCK_DIVIDER); // you can change this clock speed but DI
-   
+
+#define WLAN_SSID       ""        // cannot be longer than 32 characters!
+#define WLAN_PASS       ""
+// Security can be WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
+#define WLAN_SECURITY   WLAN_SEC_WPA2
+
+// sensors
 Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(10085);
 Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
 
@@ -33,26 +41,13 @@ void setup(void)
     while(1);
   }
   
-  /* Initialise the sensor */
-  if(!bmp.begin())
-  {
-    /* There was a problem detecting the BMP085 ... check your connections */
-//    Serial.print("Ooops, no BMP085 detected ... Check your wiring or I2C ADDR!");
-    while(1);
-  }
+  // configure sensors
   
-  configureBmp();
+  configure_bmp();
+  configure_tsl();
   
-  /* Initialise the sensor */
-  if(!tsl.begin())
-  {
-    /* There was a problem detecting the ADXL345 ... check your connections */
-//    Serial.print("Ooops, no TSL2561 detected ... Check your wiring or I2C ADDR!");
-    while(1);
-  }
-  
-  /* Setup the sensor gain and integration time */
-  configureTsl();
+  // configure wifi
+  configure_cc3000();
     
   /* We're ready to go! */
   Serial.println("");
@@ -64,20 +59,28 @@ void setup(void)
     sensor API sensor_t type (see Adafruit_Sensor for more information)
 */
 /**************************************************************************/
-void configureBmp(void)
+static void configure_bmp(void)
 {
-  sensor_t sensor;
-  bmp.getSensor(&sensor);
-  Serial.println("------------------------------------");
-  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
-  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
-  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
-  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" hPa");
-  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" hPa");
-  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" hPa");  
-  Serial.println("------------------------------------");
-  Serial.println("");
-  delay(500);
+  /* Initialise the sensor */
+  if(!bmp.begin())
+  {
+    /* There was a problem detecting the BMP085 ... check your connections */
+    Serial.print("Ooops, no BMP085 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+  
+//  sensor_t sensor;
+//  bmp.getSensor(&sensor);
+//  Serial.println("------------------------------------");
+//  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+//  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+//  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+//  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" hPa");
+//  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" hPa");
+//  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" hPa");  
+//  Serial.println("------------------------------------");
+//  Serial.println("");
+//  delay(500);
 }
 
 /**************************************************************************/
@@ -85,21 +88,28 @@ void configureBmp(void)
     Configures the gain and integration time for the TSL2561
 */
 /**************************************************************************/
-void configureTsl(void)
+static void configure_tsl(void)
 {
+  /* Initialise the sensor */
+  if(!tsl.begin())
+  {
+    /* There was a problem detecting the ADXL345 ... check your connections */
+    Serial.print("Ooops, no TSL2561 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
   
-  sensor_t sensor;
-  tsl.getSensor(&sensor);
-  Serial.println("------------------------------------");
-  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
-  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
-  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
-  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" hPa");
-  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" hPa");
-  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" hPa");  
-  Serial.println("------------------------------------");
-  Serial.println("");
-  delay(500);
+//  sensor_t sensor;
+//  tsl.getSensor(&sensor);
+//  Serial.println("------------------------------------");
+//  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+//  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+//  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+//  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" lux");
+//  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" lux");
+//  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" lux");  
+//  Serial.println("------------------------------------");
+//  Serial.println("");
+//  delay(500);
   
   /* You can also manually set the gain or enable auto-gain support */
   // tsl.setGain(TSL2561_GAIN_1X);      /* No gain ... use in bright light to avoid sensor saturation */
@@ -119,10 +129,213 @@ void configureTsl(void)
   Serial.println("------------------------------------");
 }
 
-sensors_event_t bmpEvent;
-sensors_event_t tslEvent;
+static void configure_cc3000(void)
+{
+  /* Initialise the module */
+  Serial.println(F("\nInitialising the CC3000 ..."));
+  if (!cc3000.begin())
+  {
+    Serial.println(F("Unable to initialise the CC3000! Check your wiring?"));
+    while(1);
+  }
+  
+  cc3000_displayDriverMode();
+  
+  uint16_t firmware = cc3000_checkFirmwareVersion();
+  if (firmware < 0x113) {
+    Serial.println(F("Wrong firmware version!"));
+    for(;;);
+  }
+  
+  cc3000_displayMACAddress();
+  
+  /* Optional: Get the SSID list (not available in 'tiny' mode) */
+//#ifndef CC3000_TINY_DRIVER
+//  cc3000_listSSIDResults();
+//#endif
 
-void acquireBmpEvent() {
+  /* Delete any old connection data on the module */
+  Serial.println(F("\nDeleting old connection profiles"));
+  if (!cc3000.deleteProfiles()) {
+    Serial.println(F("Failed!"));
+    while(1);
+  }
+  
+  /* Attempt to connect to an access point */
+  char *ssid = WLAN_SSID;             /* Max 32 chars */
+  Serial.print(F("\nAttempting to connect to ")); Serial.println(ssid);
+  
+  /* NOTE: Secure connections are not available in 'Tiny' mode! */
+  if (!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) {
+    Serial.println(F("Failed!"));
+    while(1);
+  }
+   
+  Serial.println(F("Connected!"));
+  
+  /* Wait for DHCP to complete */
+  Serial.println(F("Request DHCP"));
+  while (!cc3000.checkDHCP())
+  {
+    delay(100); // ToDo: Insert a DHCP timeout!
+  } 
+  
+  /* Display the IP address DNS, Gateway, etc. */  
+  while (! cc3000_displayConnectionDetails()) {
+    delay(1000);
+  }
+  
+  /* You need to make sure to clean up after yourself or the CC3000 can freak out */
+  /* the next time you try to connect ... */
+  Serial.println(F("\n\nClosing the connection"));
+  cc3000.disconnect();  
+}
+
+/**************************************************************************/
+/*!
+    @brief  Displays the driver mode (tiny of normal), and the buffer
+            size if tiny mode is not being used
+
+    @note   The buffer size and driver mode are defined in cc3000_common.h
+*/
+/**************************************************************************/
+static void cc3000_displayDriverMode(void)
+{
+  #ifdef CC3000_TINY_DRIVER
+    Serial.println(F("CC3000 is configure in 'Tiny' mode"));
+  #else
+    Serial.print(F("RX Buffer : "));
+    Serial.print(CC3000_RX_BUFFER_SIZE);
+    Serial.println(F(" bytes"));
+    Serial.print(F("TX Buffer : "));
+    Serial.print(CC3000_TX_BUFFER_SIZE);
+    Serial.println(F(" bytes"));
+  #endif
+}
+
+/**************************************************************************/
+/*!
+    @brief  Tries to read the CC3000's internal firmware patch ID
+*/
+/**************************************************************************/
+uint16_t cc3000_checkFirmwareVersion(void)
+{
+  uint8_t major, minor;
+  uint16_t version;
+  
+#ifndef CC3000_TINY_DRIVER  
+  if(!cc3000.getFirmwareVersion(&major, &minor))
+  {
+    Serial.println(F("Unable to retrieve the firmware version!\r\n"));
+    version = 0;
+  }
+  else
+  {
+    Serial.print(F("Firmware V. : "));
+    Serial.print(major); Serial.print(F(".")); Serial.println(minor);
+    version = major; version <<= 8; version |= minor;
+  }
+#endif
+  return version;
+}
+
+/**************************************************************************/
+/*!
+    @brief  Tries to read the 6-byte MAC address of the CC3000 module
+*/
+/**************************************************************************/
+void cc3000_displayMACAddress(void)
+{
+  uint8_t macAddress[6];
+  
+  if(!cc3000.getMacAddress(macAddress))
+  {
+    Serial.println(F("Unable to retrieve MAC Address!\r\n"));
+  }
+  else
+  {
+    Serial.print(F("MAC Address : "));
+    cc3000.printHex((byte*)&macAddress, 6);
+  }
+}
+
+
+/**************************************************************************/
+/*!
+    @brief  Begins an SSID scan and prints out all the visible networks
+*/
+/**************************************************************************/
+
+//static void cc3000_listSSIDResults(void)
+//{
+//  uint8_t valid, rssi, sec, index;
+//  char ssidname[33]; 
+//
+//  index = cc3000.startSSIDscan();
+//
+//  Serial.print(F("Networks found: ")); Serial.println(index);
+//  Serial.println(F("================================================"));
+//
+//  while (index) {
+//    index--;
+//
+//    valid = cc3000.getNextSSID(&rssi, &sec, ssidname);
+//    
+//    Serial.print(F("SSID Name    : ")); Serial.print(ssidname);
+//    Serial.println();
+//    Serial.print(F("RSSI         : "));
+//    Serial.println(rssi);
+//    Serial.print(F("Security Mode: "));
+//    Serial.println(sec);
+//    Serial.println();
+//  }
+//  Serial.println(F("================================================"));
+//
+//  cc3000.stopSSIDscan();
+//}
+
+/**************************************************************************/
+/*!
+    @brief  Tries to read the IP address and other connection details
+*/
+/**************************************************************************/
+bool cc3000_displayConnectionDetails(void)
+{
+  uint32_t ipAddress, netmask, gateway, dhcpserv, dnsserv;
+  
+  if(!cc3000.getIPAddress(&ipAddress, &netmask, &gateway, &dhcpserv, &dnsserv))
+  {
+    Serial.println(F("Unable to retrieve the IP Address!\r\n"));
+    return false;
+  }
+  else
+  {
+    Serial.print(F("\nIP Addr: ")); cc3000.printIPdotsRev(ipAddress);
+    Serial.print(F("\nNetmask: ")); cc3000.printIPdotsRev(netmask);
+    Serial.print(F("\nGateway: ")); cc3000.printIPdotsRev(gateway);
+    Serial.print(F("\nDHCPsrv: ")); cc3000.printIPdotsRev(dhcpserv);
+    Serial.print(F("\nDNSserv: ")); cc3000.printIPdotsRev(dnsserv);
+    Serial.println();
+    return true;
+  }
+}
+
+/**************************************************************************/
+/*
+    Arduino loop function, called once 'setup' is complete (your own code
+    should go here)
+*/
+/**************************************************************************/
+void loop(void) 
+{
+  acquireBmpEvent();
+  acquireTslEvent();
+  delay(1618);
+}
+
+static void acquireBmpEvent() {
+  sensors_event_t bmpEvent;
+
   /* BMP */
   
   bmp.getEvent(&bmpEvent);
@@ -163,8 +376,9 @@ void acquireBmpEvent() {
   }
 }
 
-void acquireTslEvent() {
-   
+static void acquireTslEvent() {
+  sensors_event_t tslEvent;
+ 
   /* TSL */
   
   tsl.getEvent(&tslEvent);
@@ -183,17 +397,5 @@ void acquireTslEvent() {
   
 }
 
-/**************************************************************************/
-/*
-    Arduino loop function, called once 'setup' is complete (your own code
-    should go here)
-*/
-/**************************************************************************/
-void loop(void) 
-{
-  acquireBmpEvent();
-  acquireTslEvent();
-  delay(1618);
-}
 
 
